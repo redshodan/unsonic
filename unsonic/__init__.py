130de85e1de1694cbdbf5355f484609689e2c983
James Newton <redshodan@gmail.com>
2013-07-20 21:57:20 -0600

    Tied the rest code into the framework.

--- unsonic/__init__.py
+++ unsonic/__init__.py
@@ -1,6 +1,12 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
+from . import rest
+
+
+PROTOCOL_VERSION = "1.10.0"
+
+
 from .models import (
     DBSession,
     Base,
@@ -17,4 +23,8 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.scan()
+    # Add the rest interfaces
+    for cmd in rest.commands.itervalues():
+        config.add_route(cmd.name, cmd.getURL())
+        config.add_view(cmd.handleReq, route_name=cmd.name)
     return config.make_wsgi_app()
