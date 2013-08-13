aaf6f907952999b9b1ec797ed5e4fe61cc2c03c6
Chris Newton <redshodan@gmail.com>
2013-08-12 18:45:41 -0600

    jamstash'ed!

--- unsonic/__init__.py
+++ unsonic/__init__.py
@@ -76,6 +76,7 @@ def main(global_config, **settings):
     # Pyramid framework
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600,)
+    config.add_static_view('jamstash', 'jamstash', cache_max_age=3600,)
     config.add_route('home', '/', factory="unsonic.views.RouteContext")
     config.scan()
 
