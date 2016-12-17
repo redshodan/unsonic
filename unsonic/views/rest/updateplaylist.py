505f3cd2d428f261d0d9418a00c9d8ae6dc7810a
Chris Newton <redshodan@users.noreply.github.com>
2016-12-16 18:23:23 -0700

    Implemented updatePlaylist.

--- unsonic/views/rest/updateplaylist.py
+++ unsonic/views/rest/updateplaylist.py
@@ -19,25 +19,41 @@ class UpdatePlayList(Command):
 
     
     def handleReq(self, session):
-        if self.params["playlistId"] and self.params["name"]:
-            raise MissingParam(
-                "Can't supply playlistId and name at the same time")
-        elif (not self.params["playlistId"] and not self.params["name"]):
-            raise MissingParam("Must supply playlistId or name")
-        
-        if self.params["playlistId"]:
-            print("Update!")
-        else:
-            plist = PlayList(user_id=self.req.authed_user.id,
-                             name=self.params["name"])
-            session.add(plist)
-            for sid in self.params["songId"]:
-                track = session.query(Track).filter(Track.id == sid).one()
-                if track is None:
-                    raise MissingParam("Invalid songId: tr-%s" % sid)
-                pltrack = PlayListTrack(track_id=track.id,
-                                        playlist_id = plist.id)
-                session.add(pltrack)
+        plist = session.query(PlayList).filter(
+            PlayList.id == self.params["playlistId"]).one_or_none()
+        if plist is None:
+            raise MissingParam("Invalid playlistId: pl-%s" % sid)
+
+        if self.params["name"]:
+            plist.name = self.params["name"]
+        if self.params["comment"]:
+            plist.comment = self.params["comment"]
+        if self.params["public"] is not None:
+            plist.public = self.params["public"]
+
+        # Delete songs
+        if len(self.params["songIndexToRemove"]):
+            tracks = session.query(PlayListTrack).filter(
+                PlayListTrack.playlist_id == self.params["playlistId"]).all()
+            tracks_len = len(tracks)
+            for index in self.params["songIndexToRemove"]:
+                if index >= tracks_len:
+                    raise MissingParam("Invalid songIndexToRemove: %d" % index)
+                res = session.query(PlayListTrack). \
+                          filter(PlayListTrack.id == tracks[index].id)
+                if not res.one_or_none():
+                    raise MissingParam("Invalid track id: tr-%s" %
+                                       tracks[index].id)
+                res.delete()
+
+        # Add songs
+        for sid in self.params["songIdToAdd"]:
+            track = session.query(Track).filter(Track.id == sid).one()
+            if track is None:
+                raise MissingParam("Invalid songId: tr-%s" % sid)
+            pltrack = PlayListTrack(track_id=track.id,
+                                    playlist_id = plist.id)
+            session.add(pltrack)
 
         return self.makeResp()
 
