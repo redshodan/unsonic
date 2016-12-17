fc7aa7a9c9f0df7e8ebdc338cb54f08249567832
Chris Newton <redshodan@gmail.com>
2016-12-16 17:43:48 -0700

    Added (stupid) update action to createPlaylist and have createPlaylist return the filled out playlist.

--- unsonic/views/rest/createplaylist.py
+++ unsonic/views/rest/createplaylist.py
@@ -1,13 +1,14 @@
 import xml.etree.ElementTree as ET
 
-from . import Command, addCmd, InternalError, MissingParam, track_t
+from . import Command, addCmd, InternalError, MissingParam, track_t, playlist_t
+from .getplaylist import GetPlayList
 from ...models import Session, PlayList, PlayListTrack, Track
 
 
 class CreatePlayList(Command):
     name = "createPlaylist.view"
     param_defs = {
-        "playlistId": {},
+        "playlistId": {"type": playlist_t},
         "name": {},
         "songId": {"multi": True, "type": track_t},
         }
@@ -20,22 +21,33 @@ class CreatePlayList(Command):
                 "Can't supply playlistId and name at the same time")
         elif (not self.params["playlistId"] and not self.params["name"]):
             raise MissingParam("Must supply playlistId or name")
-        
+
         if self.params["playlistId"]:
-            print("Update!")
+            # Update/append case
+            plist = session.query(PlayList).filter(
+                PlayList.id == self.params["playlistId"]).one_or_none()
+            if plist is None:
+                raise MissingParam("Invalid playlistId: pl-%s" % sid)
         else:
+            # New creation case
             plist = PlayList(user_id=self.req.authed_user.id,
                              name=self.params["name"])
             session.add(plist)
-            for sid in self.params["songId"]:
-                track = session.query(Track).filter(Track.id == sid).one()
-                if track is None:
-                    raise MissingParam("Invalid songId: tr-%s" % sid)
-                pltrack = PlayListTrack(track_id=track.id,
-                                        playlist_id = plist.id)
-                session.add(pltrack)
 
-        return self.makeResp()
+        # Add tracks
+        for sid in self.params["songId"]:
+            track = session.query(Track).filter(Track.id == sid).one_or_none()
+            if track is None:
+                raise MissingParam("Invalid songId: tr-%s" % sid)
+            pltrack = PlayListTrack(track_id=track.id,
+                                    playlist_id = plist.id)
+            session.add(pltrack)
+            self.params["id"] = plist.id
+
+        session.commit()
+
+        # Fun little hack to return the newly created playlist
+        return GetPlayList.handleReq(self, session)
 
 
 addCmd(CreatePlayList)
