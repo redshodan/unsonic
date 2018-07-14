3cf0a9fe8ef6db1190142593a19d5a810d8b87ae
Chris Newton <redshodan@gmail.com>
2018-07-14 09:33:32 -0600

    Added tests for getLyrics

---
+++ test/tests/rest/testgetlyrics.py
@@ -0,0 +1,14 @@
+import pytest
+
+from unsonic.views.rest.getlyrics import GetLyrics
+from . import buildCmd, checkResp
+
+
+def testGetLyrics(session):
+    cmd = buildCmd(session, GetLyrics, {"artist": "Tool", "title": "Stinkfist"})
+    checkResp(cmd.req, cmd())
+
+
+def testGetLyricsByID(session):
+    cmd = buildCmd(session, GetLyrics, {"id": "tr-1"})
+    checkResp(cmd.req, cmd())
