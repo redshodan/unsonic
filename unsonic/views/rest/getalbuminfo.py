195c94c31ddc3f26d396975419d890b7b067edac
Chris Newton <redshodan@gmail.com>
2018-12-29 10:01:08 -0700

    Handle particular lastfm errors better

--- unsonic/views/rest/getalbuminfo.py
+++ unsonic/views/rest/getalbuminfo.py
@@ -51,6 +51,13 @@ class GetAlbumInfo(Command):
             url = ET.Element("largeImageUrl")
             url.text = lf_album.get_cover_image(pylast.SIZE_EXTRA_LARGE)
             ainfo.append(url)
+        except pylast.WSError as e:
+            log.error("Error talking to LastFM: %s / %s", str(e), e.get_id())
+            # wtf is this a string and an int?
+            if e.get_id() == str(pylast.STATUS_INVALID_PARAMS):
+                return self.makeResp(status=404)
+            else:
+                return self.makeResp(status=504)
         except Exception as e:
             log.error("Error talking to LastFM: " + str(e))
             return self.makeResp(status=504)
