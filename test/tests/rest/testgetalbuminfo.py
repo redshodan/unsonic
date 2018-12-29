6874a44487d2f61b1c92a16695a38285e11378cf
Chris Newton <redshodan@gmail.com>
2018-12-29 10:51:30 -0700

    Do proper track lookup in test code

--- test/tests/rest/testgetalbuminfo.py
+++ test/tests/rest/testgetalbuminfo.py
@@ -1,7 +1,9 @@
 import pytest
 
+from unsonic.views.rest import fillID
 from unsonic.views.rest.getalbuminfo import GetAlbumInfo
 from unsonic.views.rest.getalbuminfo2 import GetAlbumInfo2
+from unsonic.models import Track
 from . import buildCmd, checkResp
 
 
@@ -11,5 +13,7 @@ def album_info(request):
 
 
 def testGetAlbumInfo(session, album_info):
-    cmd = buildCmd(session, album_info, {"id": "tr-1"})
+    row = session.query(Track).\
+        filter_by(title="Harder, Better, Faster, Stronger").one_or_none()
+    cmd = buildCmd(session, album_info, {"id": fillID(row)})
     checkResp(cmd.req, cmd(), ok504=True)
